9ca27dfbc6402b27aa997721952f7738988aeabe
hyunseo <goatsfish@gmail.com>
2022-12-06 07:51:09 +0900

    add : JdbcTemplate 이용

--- spring-jdbc/src/test/java/com/example/lecture/JDBCTest.java
+++ spring-jdbc/src/test/java/com/example/lecture/JDBCTest.java
@@ -2,6 +2,9 @@ package com.example.lecture;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -9,6 +12,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 @Slf4j
+@SpringBootTest
 public class JDBCTest {
 
     static final String JDBC_DRIVER = "org.h2.Driver";
@@ -19,6 +23,9 @@ public class JDBCTest {
     static final String DROP_TABLE_SQL = "DROP TABLE customers IF EXISTS";
     static final String CREATE_TABLE_SQL = "CREATE TABLE customers(id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))";
     static final String INSERT_SQL = "INSERT INTO customers (id, first_name, last_name) VALUES(1, 'hyunseo', 'park')";
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @Test
     void jdbc_sample() {
@@ -46,5 +53,21 @@ public class JDBCTest {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    void jdbcTemplate_sample() {
+        jdbcTemplate.update(DROP_TABLE_SQL);
+        jdbcTemplate.update(CREATE_TABLE_SQL);
+        log.info("CREATED TABLE USING JDBC TEMPLATE");
+
+        jdbcTemplate.update(INSERT_SQL);
+        log.info("INSERTED CUSTOMER USING JDBC TEMPLATE");
+
+        String fullName = jdbcTemplate.queryForObject(
+                "SELECT * FROM customers WHERE id = 1",
+                (resultSet, i) -> resultSet.getString("first_name") + " " + resultSet.getString("last_name")
+        );
+        log.info("FULL_NAME : {}", fullName);
     }
 }
