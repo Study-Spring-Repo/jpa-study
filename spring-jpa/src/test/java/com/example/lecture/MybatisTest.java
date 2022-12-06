5c622bb8588de61714c8a97e7246e77418af0f84
hyunseo <goatsfish@gmail.com>
2022-12-06 23:12:22 +0900

    init : spring-jpa

---
+++ spring-jpa/src/test/java/com/example/lecture/MybatisTest.java
@@ -0,0 +1,34 @@
+package com.example.lecture;
+
+import com.example.lecture.domain.Customer;
+import com.example.lecture.repository.CustomerMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Slf4j
+@SpringBootTest
+public class MybatisTest {
+
+    static final String DROP_TABLE_SQL = "DROP TABLE customers IF EXISTS";
+    static final String CREATE_TABLE_SQL = "CREATE TABLE customers(id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))";
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    CustomerMapper customerMapper;
+
+    @Test
+    void save_test() {
+        jdbcTemplate.update(DROP_TABLE_SQL);
+        jdbcTemplate.update(CREATE_TABLE_SQL);
+
+        customerMapper.save(new Customer(1L, "hyunseo", "park"));
+        Customer customer = customerMapper.findById(1L);
+
+        log.info("fullName : {} {}", customer.getFirstName(), customer.getLastName());
+    }
+}
