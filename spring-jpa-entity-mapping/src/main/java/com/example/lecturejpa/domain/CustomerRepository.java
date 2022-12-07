b1869d342f629faa6c3a7889137c6608f77d3c6f
hyunseo <goatsfish@gmail.com>
2022-12-07 14:14:49 +0900

    build : 빌드 파일 수정 및 도메인 삭제

--- spring-jpa-entity-mapping/src/main/java/com/example/lecturejpa/domain/CustomerRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.lecturejpa.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
